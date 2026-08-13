b92bde8e663ef7f9c0d153514a22143a00fcd2bf
kimjinhak923-blip <kimjinhak923@gmail.com>
2026-08-13 13:37:45 +0900

    Fix page layout configuration in 매출관리.py

--- pages/03_💰_매출관리.py
+++ pages/03_💰_매출관리.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.set_page_config(page_title="매출관리", page_layout="wide")
+st.set_page_config(page_title="매출관리", layout="wide")
 
 import pandas as pd
 from sidebar_menu import render_sidebar
